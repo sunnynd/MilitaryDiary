39d09b36f335a6e422572aa6ac1802ca4a17a676
sunny <dyoon0617@gmail.com>
2018-12-28 23:33:44 +0900

    Adapt the databinding

--- app/src/main/java/com/android/gotoday/home/HomeActivity.java
+++ app/src/main/java/com/android/gotoday/home/HomeActivity.java
@@ -1,15 +1,19 @@
 package com.android.gotoday.home;
 
+import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.android.gotoday.R;
+import com.android.gotoday.databinding.ActivityHomeBinding;
 
 public class HomeActivity extends AppCompatActivity {
+
+    ActivityHomeBinding mBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+        mBinding = DataBindingUtil.setContentView(this, R.layout.activity_home);
     }
 }
--- app/src/main/java/com/android/gotoday/join/JoinActivity.java
+++ app/src/main/java/com/android/gotoday/join/JoinActivity.java
@@ -1,23 +1,24 @@
 package com.android.gotoday.join;
 
 import android.content.Intent;
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.CardView;
 import android.view.View;
 
 import com.android.gotoday.R;
+import com.android.gotoday.databinding.ActivityJoinBinding;
 import com.android.gotoday.home.HomeActivity;
 
 public class JoinActivity extends AppCompatActivity {
 
+    ActivityJoinBinding mBinding;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_join);
-
-        CardView cardView = findViewById(R.id.cardview_join_finish);
-        cardView.setOnClickListener(new View.OnClickListener() {
+        mBinding = DataBindingUtil.setContentView(this, R.layout.activity_join);
+        mBinding.cardviewJoinFinish.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(view.getContext(), HomeActivity.class);
