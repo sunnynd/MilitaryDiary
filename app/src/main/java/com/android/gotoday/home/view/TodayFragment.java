c43a447a8a72b8334af2ae96455766b265a82d2e
sunny <dyoon0617@gmail.com>
2018-12-29 13:33:21 +0900

    Add edit text in diary

--- app/src/main/java/com/android/gotoday/home/view/TodayFragment.java
+++ app/src/main/java/com/android/gotoday/home/view/TodayFragment.java
@@ -7,6 +7,7 @@ import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import com.android.gotoday.R;
@@ -23,7 +24,6 @@ public class TodayFragment extends BaseFragment {
         viewPager.setAdapter(new TodayPagerAdapter(getContext()));
         TextView textView = view.findViewById(R.id.textview_base_fragment);
         textView.setText(getText());
-
         return view;
     }
 
