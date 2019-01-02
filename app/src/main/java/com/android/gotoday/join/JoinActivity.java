766c23457a0708ae7e67f6485e7afa3c5c23b990
jdragon.bae <jdragon.bae@gmail.com>
2019-01-02 18:17:48 +0900

    Add calendar ui when enter date is clicked

--- app/src/main/java/com/android/gotoday/join/JoinActivity.java
+++ app/src/main/java/com/android/gotoday/join/JoinActivity.java
@@ -1,23 +1,48 @@
 package com.android.gotoday.join;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.DatePicker;
 
 import com.android.gotoday.R;
 import com.android.gotoday.databinding.ActivityJoinBinding;
 import com.android.gotoday.home.HomeActivity;
 
+import java.util.Calendar;
+
 public class JoinActivity extends AppCompatActivity {
 
     ActivityJoinBinding mBinding;
+    DatePickerDialog datePickerDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         mBinding = DataBindingUtil.setContentView(this, R.layout.activity_join);
+
+        mBinding.edittextValueEnterDate.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                Calendar calendar = Calendar.getInstance();
+                int year = calendar.get(Calendar.YEAR);
+                int month = calendar.get(Calendar.MONTH);
+                int dayOfMonth = calendar.get(Calendar.DAY_OF_MONTH);
+                datePickerDialog = new DatePickerDialog(JoinActivity.this,
+                        new DatePickerDialog.OnDateSetListener() {
+                            @Override
+                            public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+                                mBinding.edittextValueEnterDate.setText(day + "/" + (month + 1) + "/" + year);
+                            }
+                        }, 0, 0, 0);
+                datePickerDialog.getDatePicker().setMinDate(System.currentTimeMillis());
+                datePickerDialog.show();
+            }
+        });
+
         mBinding.cardviewJoinFinish.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
